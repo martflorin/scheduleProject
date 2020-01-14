a2bc3688edc72654417e918253ef50a21ba5648b
Dudau Andreea-Codruta <andreea.dudau@info.uaic.ro>
2020-01-14 19:46:39 +0200

    exportCSV

---
+++ api/exportCSV.php
@@ -0,0 +1,15 @@
+<?php
+
+require "db.inc.php";
+
+$commData = "";
+$sql = mysqli_query($con,"SELECT*FROM alege");
+while($data = $sql->fetch_assoc())
+    $commData .= $data['id'] . ',' . $data['id_profesor'] . ',' . $data['sala'] . ',' . $data['zi'] . ',' . $data['interval_timp'] . ',' . $data['comentariu'] . "\n";
+
+$resp = "data:text/csv;charset=utf-8, ID, ID_PROF, SALA, ZI, INTERVAL, COMENTARIU\n";
+$resp .= $commData;
+
+echo '<a href="'.$resp.'" download="comm.csv">Download</a>';
+
+?>
